ea390d793a696e099bb1872784ff46a780b7fcd4
vagrant <vagrant@localhost.localdomain>
2021-11-23 06:24:38 +0000

    routeコンポーネント自作クラス化

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Listing } from './pages/listing/Listing';
-import { Contacts } from './pages/contacts/Contacts';
-import { IncomeManagement } from './pages/income_management/IncomeManagement';
-import { Manual } from './pages/manual/Manual';
-import { Setting } from './pages/setting/Setting';
-import { SignIn } from './pages/signIn/SignIn';
-import { PrivateRoute } from './PrivateRoute';
-import * as AppConstants from './commons/AppConstants';
+import { BrowserRouter} from 'react-router-dom'
+import { ReactRoutes } from './ReactRouter';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={ AppConstants.END_POINT_LISTING } element={<PrivateRoute><Listing/></PrivateRoute>}/>
-        <Route path={ AppConstants.END_POINT_CONTACTS } element={<PrivateRoute><Contacts/></PrivateRoute>}/>
-        <Route path={ AppConstants.END_POINT_INCOME_MANGEGEMENT } element={<PrivateRoute><IncomeManagement/></PrivateRoute>}/>
-        <Route path={ AppConstants.END_POINT_MANUAL } element={<PrivateRoute><Manual/></PrivateRoute>}/>
-        <Route path={ AppConstants.END_POINT_SETTING } element={<PrivateRoute><Setting/></PrivateRoute>}/>
-        <Route path={ AppConstants.END_POINT_SIGN_IN } element={<SignIn/>} />
-      </Routes>
+      <ReactRoutes/>
     </BrowserRouter>
   );
 }
