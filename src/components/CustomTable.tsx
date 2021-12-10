6d716216227279dda5b47f71ec5105dac7f4dec7
vagrant <vagrant@localhost.localdomain>
2021-12-10 17:25:24 +0000

    テーブルコンポーネント修正

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -1,11 +1,50 @@
 import { VFC, ForwardedRef, ChangeEvent, useState, forwardRef, useImperativeHandle, useCallback } from 'react';
 import { TextField, MenuItem } from '@mui/material';
 import { LocalizationProvider, DesktopDatePicker } from '@mui/lab';
+import { makeStyles } from '@mui/styles';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 
 type Props = {
   ref: ForwardedRef<CustomTableHandles>
 };
+
+const useStyles = makeStyles({
+  table: {
+    width: '100%',
+    borderRadius: '4px',
+    borderCollapse: 'collapse',
+    borderSpacing: 0,
+    minWidth: '650px',
+    overflowX: 'auto',
+    boxShadow: '0.5px 0.5px 3px gray',
+    '& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root, .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root': {
+      height: '45px!important'
+    }
+  },
+  thead: {
+    borderCollapse: 'collapse',
+    borderSpacing: 0
+  },
+  tbody: {
+    borderCollapse: 'collapse',
+    borderSpacing: 0
+  },
+  tr: {
+    borderCollapse: 'collapse',
+    borderSpacing: 0
+  },
+  th: {
+    textAlign: 'left',
+    fontSize: '0.875rem',
+    padding: '14px',
+    borderBottom: '1px solid #dcdcdc'
+  },
+  td: {
+    fontWeight: 500,
+    fontSize: '0.875rem',
+    borderBottom: '1px solid #dcdcdc'
+  },
+});
 
 export interface CustomTableHandles {
   setCols(_cols: string[]): void,
@@ -16,33 +55,67 @@ export interface CustomTableHandles {
   setWidths(widths: string[]): void,
   setIsHiddenCols(isHiddenCols: boolean[]): void,
   setIsEditCols(isEditCols: boolean[]): void,
-  setRowOnChange(rowOnChange: {　(rowIdx: number, col: string, colIdx: number) :void }): void
+  setOnChangeCell(onChangeCell: {　(rowIdx: number, col: string, colIdx: number) :void }): void
 }
 
-interface rowOnChangeCallback{(　rowIdx: number, col: string, colIdx: number) :void　};
+interface onChangeCellCallback{ (rowIdx: number, col: string, colIdx: number) :void　};
 
+/**
+ * カスタムテーブルコンポーネント
+ * 
+ * @param props プロパティ
+ * @param ref コンポーネント参照オブジェクト
+ * @returns カスタムテーブル
+ */
 export const CustomTable: VFC<Props> = forwardRef<CustomTableHandles>((props, ref) => {
+
+  const classes = useStyles();
+
+  /** テーブル列 */
   const [cols, setCols] = useState<string[]>([]);
+  /** テーブル行 */
   const [rows, setRows] = useState<{[key: string]: {}}[]>([]);
+  /** フィルター用テーブル行 */
   const [filteredRows, setFilteredRows] = useState<{[key: string]: {}}[]>([]);
+  /** コンボボックス */
   const [selects, setSelects] = useState<{[key: string]: {}}>({});
+  /** 日付入力テキスト */
   const [isDatePickers, setIsDatePickers] = useState<boolean[]>([]);
+  /** 横幅 */
   const [widths, setWidths] = useState<string[]>([]);
+  /** 非表示列 */
   const [isHiddenCols, setIsHiddenCols] = useState<boolean[]>([]);
+  /** 編集列  */
   const [isEditCols, setIsEditCols] = useState<boolean[]>([]);
-  const [rowOnChange, setRowOnChange] = useState<rowOnChangeCallback>();
+  /** セル入力イベント */
+  const [onChangeCell, setOnChangeCell] = useState<onChangeCellCallback>();
 
   useImperativeHandle(ref, () => ({
 
+    /**
+     * テーブル列の設定
+     * 
+     * @param _cols テーブル列
+     */
     setCols(_cols: string[]){
       setCols(_cols);
     },
 
+    /**
+     * テーブル行の設定
+     * 
+     * @param _rows テーブル行
+     */
     setRows(_rows: {[key: string]: {}}[]){
       setRows(_rows);
       setFilteredRows(_rows);
     },
 
+    /**
+     * フィルター用テーブル行の設定
+     * 
+     * @param regex 正規表現
+     */
     filteredRows(regex: RegExp){
       const filteredRows = rows.filter((row) => {
         return Object.keys(row).some((field) => {
@@ -52,31 +125,69 @@ export const CustomTable: VFC<Props> = forwardRef<CustomTableHandles>((props, re
       setFilteredRows(filteredRows);
     },
 
-    setSelects(objects: {[key: string]: {}}){
-      setSelects(objects);
+    /**
+     * コンボボックスの設定
+     * 
+     * @param selects コンボボックス
+     */
+    setSelects(selects: {[key: string]: {}}){
+      setSelects(selects);
     },
 
+    /**
+     * 日付入力テキストの設定
+     * 
+     * @param datePickers 日付入力テキスト
+     */
     setIsDatePickers(datePickers: boolean[]){
       setIsDatePickers(datePickers);
     },
 
+    /**
+     * 横幅の設定
+     * 
+     * @param widths 横幅
+     */
     setWidths(widths: string[]){
       setWidths(widths);
     },
 
-    setIsHiddenCols(filteredCols: boolean[]){
-      setIsHiddenCols(filteredCols);
+    /**
+     * 非表示列の設定
+     * 
+     * @param hiddenCols 非表示列
+     */
+    setIsHiddenCols(hiddenCols: boolean[]){
+      setIsHiddenCols(hiddenCols);
     },
 
+    /**
+     * 編集列の設定
+     * 
+     * @param isEditCols 編集列
+     */
     setIsEditCols(isEditCols: boolean[]){
       setIsEditCols(isEditCols);
     },
 
-    setRowOnChange(rowOnChange: rowOnChangeCallback){
-      setRowOnChange(rowOnChange);
+    /**
+     * セル入力イベントの設定
+     * 
+     * @param onChangeCell セル入力イベント
+     */
+    setOnChangeCell(onChangeCell: onChangeCellCallback){
+      setOnChangeCell(onChangeCell);
     }
   }));
 
+  /**
+   * 行の更新
+   * 
+   * @param rowIdx 行番号
+   * @param col 列
+   * @param colIdx 列番号
+   * @param value 入力値
+   */
   const updateRows = useCallback((rowIdx: number, col: string, colIdx: number, value: string | Date | null) => {
 
     if(!value){
@@ -91,36 +202,51 @@ export const CustomTable: VFC<Props> = forwardRef<CustomTableHandles>((props, re
     _renderRows[rowIdx] = { ..._renderRows[rowIdx], [col]: value }
     setFilteredRows(_renderRows);
 
-    if(rowOnChange){
-      rowOnChange(rowIdx, col, colIdx);
+    if(onChangeCell){
+      onChangeCell(rowIdx, col, colIdx);
     }
-  }, [rows, filteredRows, setRows, setFilteredRows, rowOnChange]);
+  }, [rows, filteredRows, setRows, setFilteredRows, onChangeCell]);
 
+  /**
+   * テキストボックスの入力イベント
+   * 
+   * @param rowIdx 行番号
+   * @param col 列
+   * @param colIdx 列番号
+   */
   const onChangeTextHandler = useCallback((rowIdx: number, col: string, colIdx: number) => (e: ChangeEvent<HTMLInputElement>) =>  {
     updateRows(rowIdx, col, colIdx, e.target.value);
   }, [updateRows]);
 
+  /**
+   * 日付入力テキストの入力イベント
+   * 
+   * @param rowIdx 行番号
+   * @param col 列
+   * @param colIdx 列番号
+   * @param value 入力値
+   */
   const onChangeDatePickerHandler = useCallback((rowIdx: number, col: string, colIdx: number, value: Date | null) => {
     updateRows(rowIdx, col, colIdx, value);
   }, [updateRows]);
 
 
   return (
-    <table className="table">
-      <thead className="thead">
-        <tr className="tr">
+    <table className={classes.table}>
+      <thead className={classes.thead}>
+        <tr className={classes.tr}>
           {cols.map((col, colIdx) => (
             isHiddenCols.length > 0 && isHiddenCols[colIdx] &&
-            <th className="th" style={ widths && { width: widths[colIdx] } } key={`head_row_${colIdx}`}>{col}</th>
+            <th className={classes.th} style={ widths && { width: widths[colIdx] } } key={`head_row_${colIdx}`}>{col}</th>
           ))}
         </tr>
       </thead>
-      <tbody className="tbody">
+      <tbody className={classes.tbody}>
         {filteredRows.map((row, rowIdx) => (
-          <tr className="tr" key={`body_row_${rowIdx}`}>
+          <tr className={classes.tr} key={`body_row_${rowIdx}`}>
             {Object.keys(row).map((col, colIdx) => (
               isHiddenCols.length > 0 && isHiddenCols[colIdx] &&
-                <td className="td" key={`col_${rowIdx}_${colIdx}`} style={ widths && { width: widths[colIdx] ,padding: isEditCols[colIdx] ? '8px 14px' : '14px' } }>
+                <td className={classes.td} key={`col_${rowIdx}_${colIdx}`} style={ widths && { width: widths[colIdx], padding: isEditCols[colIdx] ? '8px 14px' : '14px' } }>
                   {isEditCols[colIdx]
                     ? col in selects
                       ? <TextField
